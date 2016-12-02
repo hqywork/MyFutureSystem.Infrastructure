deb964a31155aa4afe5b2a71fc1ba461067b2ab1
张奇 <hqywork@163.com>
2016-12-02 23:06:56 +0800

    初始化解决方案及项目结构

---
+++ MyFutureInfrastructure/Src/HQY.Future.Infrastructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("ebb5d93d-0213-456e-82af-cb883501d2cc")]
---
+++ MyFutureInfrastructure/UnitTest/HQY.Future.Infrastructure.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("afc5212d-1059-4017-a022-74a403885e78")]
