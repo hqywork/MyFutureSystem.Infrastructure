6add881bab0bfa2c92ee653e37dbe63eab5e935c
张奇 <hqywork@163.com>
2016-12-06 22:01:49 +0800

    添加新的项目 “HQY.Common”，用于存放通用公共功能。

---
+++ MyFutureInfrastructure/Src/HQY.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7049d0ef-43a3-4416-ab10-926521eb4ba3")]
---
+++ MyFutureInfrastructure/UnitTest/HQY.Common.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9b52eb22-c36c-4c53-ba9a-22346c1b1abb")]
