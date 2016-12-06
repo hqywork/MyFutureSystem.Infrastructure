3860cc2f41704cf454411d2d4d8c1d4b0412d0ac
张奇 <hqywork@163.com>
2016-12-06 22:34:05 +0800

    新增 “可枚举数接口” 扩展方法，实现 ForEach 处理。

---
+++ MyFutureInfrastructure/Src/HQY.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,58 @@
+﻿/**********************************************************************************************************************
+ * 变更历史：
+ *      作者：张奇  时间：2016年12月06日	新建	
+ * 
+ *********************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable CheckNamespace
+namespace HQY.Common
+// ReSharper restore CheckNamespace
+{
+    /// <summary>
+    /// 包含 <see cref="IEnumerable{T}"/> 扩展方法的静态类。
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// 循环对 <paramref name="source"/> 内的每个元素执行 <paramref name="action"/> 定义的操作。
+        /// </summary>
+        /// <typeparam name="T"><paramref name="source"/> 中元素的类型。</typeparam>
+        /// <param name="source">包含将要处理元素的 <see cref="IEnumerable{T}"/>。</param>
+        /// <param name="action">将要应用于元素的方法。</param>
+        /// <exception cref="NullReferenceException">当 <paramref name="source"/> 为空引用（<c>null</c>）时引发。</exception>
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            #region Param Check
+
+            ParamCheck.NotNull(source, nameof(source));
+
+            #endregion
+
+            foreach (T item in source)
+                action.Invoke(item);
+        }
+
+        /// <summary>
+        /// 循环对 <paramref name="source"/> 内的每个元素执行 <paramref name="func"/> 定义的操作，并返回包含处理后元素的 <see cref="IEnumerable{T}"/>。
+        /// </summary>
+        /// <typeparam name="T"><paramref name="source"/> 中元素的类型。</typeparam>
+        /// <param name="source">包含将要处理元素的 <see cref="IEnumerable{T}"/>。</param>
+        /// <param name="func">将要应用于元素的方法。</param>
+        /// <returns>包含处理后元素的 <see cref="IEnumerable{T}"/>。</returns>
+        /// <exception cref="NullReferenceException">当 <paramref name="source"/> 为空引用（<c>null</c>）时引发。</exception>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Func<T, T> func)
+        {
+            #region Param Check
+
+            ParamCheck.NotNull(source, nameof(source));
+
+            #endregion
+
+            return source.Select(item => func.Invoke(item)).ToList();
+        }
+    }
+}
---
+++ MyFutureInfrastructure/UnitTest/HQY.Common.Test/Extensions/EnumerableExtensionsTest.cs
@@ -0,0 +1,63 @@
+﻿/**********************************************************************************************************************
+ * 变更历史：
+ *      作者：张奇  时间：2016年12月06日	新建	
+ * 
+ *********************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace HQY.Common.Test.Extensions
+{
+    /// <summary>
+    /// <see cref="EnumerableExtensions"/> 单元测试。
+    /// </summary>
+    public class EnumerableExtensionsTest
+    {
+        /// <summary>
+        /// <see cref="EnumerableExtensions.ForEach{T}(IEnumerable{T}, Action{T})"/> 单元测试。
+        /// </summary>
+        [Theory(DisplayName = "IEnumerable{T}.ForEachByAction")]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, null, new[] { 1, 4, 9, 16, 25 })]
+        [InlineData(null, typeof(ArgumentNullException), null)]
+        public void ForEachByActionTest(IEnumerable<Int32> value, Type exType, IEnumerable<Int32> result)
+        {
+            if (exType != null)
+            {
+                ArgumentException ex =
+                    (ArgumentException)
+                    Assert.Throws(exType, () => EnumerableExtensions.ForEach(value, (ele)=> { }));
+                Assert.Equal("source", ex.ParamName);
+            }
+            else
+            {
+                IList<Int32> tmp = new List<Int32>();
+                value.ForEach(ele => tmp.Add(ele * ele));
+                Assert.Equal(result, tmp);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="EnumerableExtensions.ForEach{T}(IEnumerable{T}, Func{T, T})"/> 单元测试。
+        /// </summary>
+        [Theory(DisplayName = "IEnumerable{T}.ForEachByFunc")]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, null, new[] { 1, 4, 9, 16, 25 })]
+        [InlineData(null, typeof(ArgumentNullException), null)]
+        public void ForEachByFuncTest(IEnumerable<Int32> value, Type exType, IEnumerable<Int32> result)
+        {
+            if (exType != null)
+            {
+                ArgumentException ex =
+                    (ArgumentException)
+                    Assert.Throws(exType, () => EnumerableExtensions.ForEach(value, ele => ele * ele));
+                Assert.Equal("source", ex.ParamName);
+            }
+            else
+            {
+                var tmp = value.ForEach(ele => ele * ele);
+                Assert.Equal(result, tmp);
+            }
+        }
+    }
+}
