381bd81e729c40f68a871336483f1aefecb3ca69
张奇 <hqywork@163.com>
2016-12-08 22:45:50 +0800

    新增 “本地调试 NuGet 包发布脚本”

---
+++ MyFutureInfrastructure/Build-Common-for-Local-Debug.cmd
@@ -0,0 +1,29 @@
+@ECHO OFF
+
+CHCP 65001>NUL
+cd %~dp0
+
+SETLOCAL
+
+REM 在运行前需修改此项，指示生成 NuGet 包的版本号。
+SET VERSION=161208
+SET PROJECT=Src\HQY.Common
+SET PackagePath=D:\Codes\NuGetPackageForDebug
+
+REM 设置 dotnet.exe 的路径
+SET DotnetCmdPath="%ProgramFiles%\dotnet\dotnet.exe"
+IF NOT EXIST %DotnetCmdPath% SET DotnetCmdPath="%ProgramFiles(x86)%\dotnet\dotnet.exe"
+IF NOT EXIST %DotnetCmdPath% SET DotnetCmdPath="%ProgramW6432%\dotnet\dotnet.exe"
+
+IF NOT EXIST %PackagePath% MD %PackagePath%
+
+ECHO 正在恢复项目依赖项以及工具 ...
+%DotnetCmdPath% restore --disable-parallel
+
+ECHO 正在生成项目（%PROJECT%） ...
+%DotnetCmdPath% build "%PROJECT%" --no-incremental --version-suffix %VERSION%
+
+ECHO 正在发布 NuGet 包（%PROJECT%.0.1.0-beta-%VERSION%）...
+%DotnetCmdPath% pack "%PROJECT%" --no-build --output "%PackagePath%" --version-suffix %VERSION%
+
+pause;
